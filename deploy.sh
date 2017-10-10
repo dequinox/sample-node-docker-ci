754565639758b560b3fdd193b4e3aee703f5416d
root <root@atreides.localdomain>
2017-10-10 23:55:05 +0300

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push alnair/sample-node
+sudo docker push alnair/sample-node
 
 ssh alikhil@35.187.72.88 << EOF
-docker pull alnair/sample-node:latest
-docker stop web || true
-docker rm web || true
-docker rmi alnair/sample-node:current || true
-docker tag alnair/sample-node:latest alnair/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 alnair/sample-node:current
+sudo docker pull alnair/sample-node:latest
+sudo docker stop web || true
+sudo docker rm web || true
+sudo docker rmi alnair/sample-node:current || true
+sudo docker tag alnair/sample-node:latest alnair/sample-node:current
+sudo docker run -d --net app --restart always --name web -p 80:80 alnair/sample-node:current
 EOF
